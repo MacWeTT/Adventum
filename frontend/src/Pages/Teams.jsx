95a11b2c364b5856851c84c6b614384a6c06baee
Akshit sharma <akshitsharmaa7322@gmail.com>
2023-06-17 20:58:39 +0530

    team page added

--- frontend/src/Pages/Teams.jsx
+++ frontend/src/Pages/Teams.jsx
@@ -1,87 +1,76 @@
 import React from "react";
-import eventsdummy from '../Assets/eventdummy.png'
+import teamsdummy from '../Assets/teamphoto2.png'
 
 const Teams = () => {
   return (
     <section className="teams bg-primary">
         <div className=" flex items-center justify-center w-full border-y-2 border-secondary">
-          <h1 className='font-bold text-3xl text-secondary py-2 md:py-6'>Teams</h1>
+          <h1 className='font-400 text-6xl text-secondary py-2 md:py-6'> OUR TEAM</h1>
+        </div>
+        <div className=" flex items-center justify-center w-full border-y-2 border-secondary">
+          <h1 className='font-300 text-3xl text-secondary py-2 md:py-6'> COUNCIL </h1>
         </div>
         <div className="teamsBody flex justify-center items-center">
-                <div className="font-bold md:text-3xl text-secondary rotate-[-90deg]">
+                <div className="font-400 lg:text-5xl md:text-3xl text-secondary rotate-[-90deg]">
                   COUNCIL
                 </div>
                 <div className="teamsLayout grid grid-cols-1 md:grid-cols-3 ">
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                      <div className="border-x-2 border-b-2 border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                      <div className="border-x-2 border-b-2 border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                      <div className="border-x-2 border-b-2  border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                  
+                      <div className="border-x-2   border-b-2 border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                      <div className="border-x-2  border-b-2 border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
-                      <div className="">
-                        <img src={eventsdummy} alt="Event Image" />
+                      <div className="border-x-2  border-b-2 border-secondary grayscale hover:grayscale-0">
+                        <img src={teamsdummy} alt="Event Image" />
                       </div>
                 </div>
-                <div className="font-bold md:text-3xl text-secondary rotate-[-90deg]">
+                <div className="font-400 lg:text-5xl md:text-3xl text-secondary rotate-[-90deg]">
                   COUNCIL
                 </div>
         </div>
         <div className="flex items-center justify-center w-full border-y-2 border-secondary">
-          <h1 className='font-bold text-3xl text-secondary py-2 md:py-6'>Executives</h1>
+          <h1 className='font-300 text-4xl text-secondary py-2 md:py-6'>EXECUTIVES</h1>
         </div>
       
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-3 p-4">
-
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-3 p-20">
+          <div className="text-secondary flex flex-col items-center justify-center font-200 text-2xl ">
+            <p>Akshit</p>
+            <p>Vidhi</p>
+            <p>Manas</p>
+            <p>Pranjal</p> 
+            <p>Aayush</p>
+            <p>Arpit</p>
+            <p>Vidushi</p>            
+          </div>  
+          <div className="text-secondary flex flex-col items-center justify-center font-200 text-2xl">
+            <p>Manas</p>
+            <p>Arnav</p>
+            <p>Aparna</p>
+            <p>Nikhilesh</p>
+            <p>Harshit</p>
+            <p>Shashank</p>
+            <p>Ankush</p>
+            
           </div>
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-          </div>
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-          </div>
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-          </div>
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-          </div>
-          <div className="text-secondary flex flex-col items-center justify-center">
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
-            <p>XYZ</p>
+          <div className="text-secondary flex flex-col items-center justify-center font-200 text-2xl px-12">
+            <p>Amritansh</p>
+            <p>Aman</p>
+            <p>Ritwija</p>
+            <p>Shashwat</p>
+            <p>Manav</p>
+            <p>Suvrat</p>
+            <p>Yatharth</p>
           </div>
         </div>
     </section>
