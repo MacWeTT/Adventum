cd0019a5a55402d4b001da8351943d25eb3fe230
Pranjal Thakur <pranjallt16@gmail.com>
2023-06-18 19:46:34 +0530

    Sponsors page

---
+++ frontend/src/Pages/Sponsors.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+// import './Sponsors.css';
+
+const Sponsors = () => {
+  return (
+    <div className="bg-primary">
+      <div className="text-gray-300 text-left font-bebas-neue text-7xl pl-52"><br />GOLD SPONSOR<br /></div><br /><br />
+      <div className="border-0 h-1 bg-white mt-0"></div>
+      <div className="relative inline-block">
+        <div className="flex justify-center">
+          <div className="border-l-1 border-white ml-250px h-800px absolute top-0 left-0 w-full z-negative"></div>
+          <div className="border-l-1 border-white ml-1100px h-400px absolute top-0 left-0 w-full z-negative"></div>
+          <div className="w-full h-500px text-center pt-50px ml-400px">
+            <img src="https://staticg.sportskeeda.com/editor/2023/02/29816-16766374114951-1920.jpg?w=840" alt="Gold Sponsor" />
+          </div>
+          <br />
+        </div>
+
+        <div className="text-gray-300 text-right font-bebas-neue font-sans text-7xl pr-52 mt-20">SILVER SPONSOR</div>
+
+        <div className="absolute top-55p left-0 w-full h-1 z-negative bg-white"></div>
+        <div className="relative inline-block">
+          <div className="border-l-1 border-white ml-250 h-400 ml-900 absolute top-75p left-0 w-0 z-negative bg-white"></div>
+
+          <div className="flex justify-center p-100px mb-0 pl-150px">
+            <div className="text-left mr-80 mt-neg-50">
+              <img src="https://preview.redd.it/yamada-kun-to-lv999-no-koi-wo-suru-my-lv999-love-for-yamada-v0-bpln7rdp3zha1.png?auto=webp&s=b9dec60076eb4c446c57a1a8694ab69d582bece3" width={400} height={600} alt="Silver Sponsor" />
+            </div>
+            <div className="text-center mr-80 mt-neg-50">
+              <img src="https://preview.redd.it/yamada-kun-to-lv999-no-koi-wo-suru-my-lv999-love-for-yamada-v0-bpln7rdp3zha1.png?auto=webp&s=b9dec60076eb4c446c57a1a8694ab69d582bece3" width={400} height={600} alt="Silver Sponsor" />
+            </div>
+            <div className="text-right mr-90 mt-neg-50">
+              <img src="https://preview.redd.it/yamada-kun-to-lv999-no-koi-wo-suru-my-lv999-love-for-yamada-v0-bpln7rdp3zha1.png?auto=webp&s=b9dec60076eb4c446c57a1a8694ab69d582bece3" width={400} height={600} alt="Silver Sponsor" />
+            </div>
+          </div>
+        </div>
+
+        <div className="text-gray-300 text-left font-bebas-neue text-7xl pl-150">PLATINUM SPONSOR<br /></div>
+        <div className="relative inline-block">
+          <div className="border-l-1 border-white ml-250 h-560 ml-600 absolute top-70p left-0 w-0 z-negative bg-white"></div>
+          <div className="absolute top-50p left-0 w-20p h-1 z-negative bg-white ml-500"></div>
+          <div className="border-10 h-1 w-35p bg-white ml-620 absolute top-226p left-0 z-negative"></div>
+          <div className="border-l-1 border-white h-350 ml-1185 absolute top-226p left-0 w-0 z-negative bg-white"></div>
+          <div className="flex justify-center p-100 pl-50">
+            <div className="text-left mr-75 pl-100">
+              <img src="https://lostinanime.com/wp-content/uploads/2023/04/Yamada-kun-to-Lv999-no-Koi-o-Suru-04-41.jpg" width={500} height={300} alt="Platinum Sponsor" />
+            </div>
+            <div className="text-left mr-75">
+              <img src="https://lostinanime.com/wp-content/uploads/2023/04/Yamada-kun-to-Lv999-no-Koi-o-Suru-04-41.jpg" width={500} height={300} alt="Platinum Sponsor" />
+            </div>
+            <div className="text-left mr-75">
+              <img src="https://lostinanime.com/wp-content/uploads/2023/04/Yamada-kun-to-Lv999-no-Koi-o-Suru-04-41.jpg" width={500} height={300} alt="Platinum Sponsor" />
+            </div>
+            <div className="text-left mr-100">
+              <img src="https://lostinanime.com/wp-content/uploads/2023/04/Yamada-kun-to-Lv999-no-Koi-o-Suru-04-41.jpg" width={500} height={300} alt="Platinum Sponsor" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-start">
+        <div className="border-l-1 border-white w-39.5 h-1 mr-330 bg-gray-100 mt-30"></div>
+        <div className="text-right text-gray-300 font-bebas-neue text-7xl pr-200">BRONZE SPONSOR<br /></div>
+      </div>
+
+      <div className="flex justify-center p-100">
+        <div className="text-left mr-75">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="text-left mr-75">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="text-left mr-75">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="bimg4">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+      </div>
+
+      <div className="flex justify-center">
+        <div className="text-left mr-75 mb-30">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="text-left mr-75 mb-30">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="text-left mr-75 mb-30">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+        <div className="bimg8">
+          <img src="https://img1.ak.crunchyroll.com/i/spire2/fd5a4ffc51225e4eaf63d4402a2e07481678277760_full.jpg" alt="Bronze Sponsor" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sponsors;
