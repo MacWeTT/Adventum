ff770ebdfdcf4793ced491f73f2ea3c2c33b52bc
Aayush Girdhar <aayush.girdhar2@gmail.com>
2023-06-22 19:11:26 +0530

    completed home page

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,36 +1,33 @@
-// import react from "react";
-import Navbar from "../Components/Navbar";
+import React from "react";
+// import Navbar from "../Components/Navbar";
 // import Footer from "../Components/Footer";
-import logo from "../Assets/adventum_logo.svg"
-import loader from "../Assets/loader.gif"
+import logo from "../Assets/adventum_logo.svg";
+import loader from "../Assets/loader.gif";
 import "../Components/CSS/Home.css";
 const Home = () => {
   return (
     <>
-      <section className="hero-section bg-primary h-screen flex flex-col justify-center items-center gap-12 md:gap-12">
-        <h1 className="hero-header font-secondary text-center pt-12 font-[700] md:text-6xl   text-4xl">the adventure awaits you</h1>
-        <p className="hero-subheader font-secondary text-center font-[400] md:text-4xl text-xl  md:w-3/5">
+      <section className="hero-section">
+        <h1 className="hero-header font-secondary">the adventure awaits you</h1>
+        <p className="hero-subheader font-secondary">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
           iste corrupti culpa.
         </p>
         <img src={logo} alt="adventum logo" className="adventum-logo" />
       </section>
-
-
-
       <div className="gradient-90deg"></div>
 
-      <section className="about-section bg-primary flex flex-col justify-center items-center gap-12">
-      </section>
-
-      {/* <article className="introduction-section font-secondary">
+      <article className="introduction-section">
         <div id="section-1">
           <div className="box-1"></div>
-          <div className="box-2"></div>
+          <div className="box-2">
+            <div className="square-decor square-2"></div>
+            <div className="square-decor square-1"></div>
+          </div>
           <div className="box-3"></div>
         </div>
         <div id="section-2">
-          <h2 className="introduction-header">
+          <h2 className="introduction-header font-secondary">
             a little introduction about us
           </h2>
           <p className="introduction-content font-secondary">
@@ -41,25 +38,47 @@ const Home = () => {
           </p>
         </div>
         <div id="section-3">
-          <div className="newbox-1">
-            <img
-              src={loader}
-              alt="loading animation"
-              className="loader"
-            />
+          <div className="loader-section">
+            <img src={loader} alt="loading animation" className="loader" />
+            <div className="square-decor square-3"></div>
+            <div className="square-decor square-4"></div>
           </div>
-          <div className="newbox-2">
-            <div className="circle c1"></div>
-            <div className="circle c2"></div>
-            <div className="circle c3"></div>
+          <div className="circle-section">
+            <div className="circle-1"></div>
+            <div className="circle-2"></div>
+            <div className="circle-3"></div>
           </div>
         </div>
-      </article> */}
+      </article>
 
       <div className="gradient-0deg"></div>
 
-      <section className="hint-section bg-primary flex flex-col justify-center items-center gap-12">
-      </section>
+      <article className="introduction-section hint">
+        <div id="section-1">
+          <div className="box-1"></div>
+          <div className="box-2">
+            <div className="square-decor square-2"></div>
+            <div className="square-decor square-1"></div>
+          </div>
+          <div className="box-3"></div>
+        </div>
+        <div id="section-2">
+          <h2 className="introduction-header font-secondary crossword-header">
+            what do we have more?
+          </h2>
+          <div className="crossword">insert crossword here</div>
+        </div>
+        <div id="hint-carousel">
+          <h2 className=" introduction-header crossword-hint-header font-secondary">
+            hint
+          </h2>
+          <p className="introduction-content font-secondary">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
+            itaque praesentium recusandae?
+          </p>
+          <button className="learn-more-btn">Learn more</button>
+        </div>
+      </article>
     </>
   );
 };
