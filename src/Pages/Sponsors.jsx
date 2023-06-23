45bd30d87f8785b0134faf9e0cad2756b2891cd4
Akshit sharma <akshitsharmaa7322@gmail.com>
2023-06-23 23:10:04 +0530

    minor changes

--- src/Pages/Sponsors.jsx
+++ src/Pages/Sponsors.jsx
@@ -34,11 +34,11 @@ const Sponsor = () => {
                 </div>
             </div>
             <div className='w-full flex'>
-                <div className='w-1/2 border-r-[1px] py-12 px-2'>
-                    <h1 className="text-4xl text-right text-secondary font-semibold">PLATINUM</h1>
+                <div className='w-1/2 border-r-[1px] py-12 lg:px-2'>
+                    <h1 className="text-3xl lg:text-4xl text-right text-secondary font-semibold">PLATINUM</h1>
                 </div>
                 <div className='w-1/2 py-12 px-2'>
-                    <h1 className="text-4xl text-left text-secondary font-semibold">SPONSOR</h1>
+                    <h1 className="text-3xl lg:text-4xl text-left text-secondary font-semibold">SPONSOR</h1>
                 </div>
             </div>
             <div className='w-4/5 grid grid-cols-1 md:grid-cols-4 gap-6 border-[1px] border-secondary p-6'>
